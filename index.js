4df0a983515f48cd5c0d89e6bb31191540d6bdb8
David <digital0david@gmail.com>
2019-05-16 21:55:37 -0400

    env file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // code away!
-require('dotenv').config
+require('dotenv').config();
 
-const server = require('./api/server.js');
+const server = require('./server.js');
 
 const port =  process.env.PORT || 5000;
 
